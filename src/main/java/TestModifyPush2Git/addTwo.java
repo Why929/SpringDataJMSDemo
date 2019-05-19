b60b5fa66a1bc6892f2e1738dec442722dab2614
Why929 <why4643@163.com>
2019-05-19 20:01:32 +0800

    组员B:修改了addTwo中输出注释

--- src/main/java/TestModifyPush2Git/addTwo.java
+++ src/main/java/TestModifyPush2Git/addTwo.java
@@ -3,6 +3,6 @@ package TestModifyPush2Git;
 public class addTwo {
     public static void main(String[] args) {
         System.out.println("这次是在addTwo文件上右键 add commit push" +
-                "");
+                "仅在被修改文件处 显示了备注文件信息");
     }
 }
