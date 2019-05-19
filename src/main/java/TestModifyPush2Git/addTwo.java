a5e8f0e9b3a84410affddcb6452d39fe8f701c31
Why929 <why4643@163.com>
2019-05-19 19:54:01 +0800

    组员B:添加了一个addTwo文件

---
+++ src/main/java/TestModifyPush2Git/addTwo.java
@@ -0,0 +1,8 @@
+package TestModifyPush2Git;
+
+public class addTwo {
+    public static void main(String[] args) {
+        System.out.println("这次是在addTwo文件上右键 add commit push" +
+                "");
+    }
+}
